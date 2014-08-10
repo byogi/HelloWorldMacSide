eea4b44ef627b9181293e7e7e263d35c5705f92a
byogi <byogi@hawaii.edu>
2014-08-09 19:41:24 -1000

    tesr
    rawr

--- HelloWorldMac/src/HelloWorldMac.java
+++ HelloWorldMac/src/HelloWorldMac.java
@@ -2,6 +2,6 @@
 public class HelloWorldMac {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello World Mac Side");
+		System.out.println("Hello World Mac Side test windows");
 	}
 }
